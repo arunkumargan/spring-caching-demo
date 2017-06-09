bd12bdde571e53af4899ddfbd4dde166c9ace6f4
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-06-09 06:35:56 -0700

    Polish

--- caching-app/src/main/java/sample/cache/config/RedisCacheManagerConfig.java
+++ caching-app/src/main/java/sample/cache/config/RedisCacheManagerConfig.java
@@ -2,10 +2,12 @@ package sample.cache.config;
 
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.List;
@@ -25,6 +27,17 @@ public class RedisCacheManagerConfig {
             cacheManager.setCacheNames(cacheNames);
         }
         return cacheManager;
+    }
+    
+    @Configuration
+    @Profile("cloud")
+    static class CloudConfig extends AbstractCloudConfig {
+        
+        @Bean
+        public RedisConnectionFactory redisFactory() {
+            return connectionFactory().redisConnectionFactory();
+        }
+        
     }
     
 }
